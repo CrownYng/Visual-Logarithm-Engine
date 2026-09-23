d82cc4c5eddbcd89eb8ceeebe09ef0c4beca0c78
CrownYng <nuggetsofgenius@gmail.com>
2026-09-23 14:33:12 +0000

    Implementation of frexp,function decomposeNum to get the double constant and exponent of 2 that gives ln(input)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
+#include <cmath>
+int decomposeNum(int num);
 int main(){
+    int num;
     
+   
+     
+    std::cout<<"Input a number to find its ln value: ";
+    std::cin>>num;
+    decomposeNum(num);
 }
+int decomposeNum( int num){
+    
+    
+    int exp;
+    double mantissa=std::frexp(num,&exp);
+    std::cout<<"The mantissa value is: " <<mantissa <<"\n";
+    std::cout<<"The standard exponent is: " <<exp <<"\n";
+    std::cout<<"Ln(" <<num <<") is  " <<"ln(2 exponent " << exp <<") ×  " <<"ln(" <<mantissa<<")";
+    
+    return mantissa,exp;
+}
+    
